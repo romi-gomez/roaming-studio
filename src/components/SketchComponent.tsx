a50fc220953451f6c741b5a581807f1f9e4e5859
Romi Gomez <contact@romigomez.com.ar>
2024-10-30 12:35:33 -0300

    chore(deps): update project dependencies to latest versions

--- src/components/SketchComponent.tsx
+++ src/components/SketchComponent.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 
-const Sketch = dynamic(import("react-p5"), { ssr: false });
+// Importa react-p5 solo en el cliente
+const Sketch = dynamic(() => import("react-p5"), { ssr: false });
 
 interface SketchComponentProps {
   setup: any;
-  draw:any;
+  draw: any;
 }
 
 const SketchComponent: React.FC<SketchComponentProps> = (props) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Cambia el estado a true en el montaje del cliente
+  }, []);
+
+  if (!isClient) return null; // Evita la renderización en el servidor
+
   return <Sketch setup={props.setup} draw={props.draw} />;
 };
 
