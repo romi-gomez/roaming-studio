4a3080489d9a5dfd19e0da5d491041d479b4f800
Romi Gomez <contact@romigomez.com.ar>
2024-10-30 12:57:44 -0300

    fix(sketches): cast elements to HTMLElement to resolve offsetWidth TypeScript error

--- src/sketches/sketch04.ts
+++ src/sketches/sketch04.ts
@@ -8,8 +8,9 @@ export const setup = (p5: p5Type, canvasParentRef: Element): void => {
   r= p5.random(255)
   g= p5.random(255)
   b= p5.random(255)
-  const frameWidth = document.getElementsByClassName('react-p5')[0].offsetWidth
-  const frameHeight = document.getElementsByClassName('react-p5')[0].offsetHeight
+  const frameElement = document.getElementsByClassName("react-p5")[0] as HTMLElement;
+  const frameWidth = frameElement.offsetWidth;
+  const frameHeight = frameElement.offsetHeight;
 
   p5.createCanvas(frameWidth, frameHeight).parent(canvasParentRef)
   console.log(p5)
